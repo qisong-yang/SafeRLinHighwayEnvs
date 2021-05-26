50f410a74ac5d721919479abe64f3b15545386cb
Danial Kamran <danial.kamran@kit.edu>
2021-05-26 10:31:48 +0200

    reduce max_ep_len

--- scripts/normal_ppo.py
+++ scripts/normal_ppo.py
@@ -4,4 +4,4 @@ import highway_env
 
 
 #ppo_lagrangian(env_fn = lambda : gym.make("merge-v0"),ac_kwargs = dict(hidden_sizes=(64,64)))
-ppo(env_fn = lambda : gym.make("merge-v0"),ac_kwargs = dict(hidden_sizes=(64,64)), logger_kwargs = {"output_dir": "./merging-04"})
+ppo(env_fn = lambda : gym.make("merge-v0"),ac_kwargs = dict(hidden_sizes=(64,64)), logger_kwargs = {"output_dir": "./fix_speed_01"}, render=False, max_ep_len=70)
