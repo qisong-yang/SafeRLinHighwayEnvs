6ae680fa10838d002ddf3b4e94b3635a7e027b8e
Danial Kamran <danial.kamran@kit.edu>
2021-05-27 10:17:54 +0200

    add lagnrangian agent

---
+++ scripts/penalized_ppo.py
@@ -0,0 +1,7 @@
+from safe_rl import ppo_lagrangian, ppo
+import gym
+import highway_env
+
+
+#ppo_lagrangian(env_fn = lambda : gym.make("merge-v0"),ac_kwargs = dict(hidden_sizes=(64,64)))
+ppo_lagrangian(env_fn = lambda : gym.make("merge-v0"),ac_kwargs = dict(hidden_sizes=(64,64)), logger_kwargs = {"output_dir": "./fix_speed_01"}, render=True, max_ep_len=70, epochs=150, steps_per_epoch=5000)
