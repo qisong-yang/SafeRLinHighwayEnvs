7d0c8a7f020207d9e6cad0ad1fb57589d3949d8b
Danial Kamran <danial.kamran@kit.edu>
2021-06-02 09:28:57 +0200

    set penalty init value for normal ppo

--- scripts/normal_ppo.py
+++ scripts/normal_ppo.py
@@ -4,4 +4,4 @@ import highway_env
 
 
 #ppo_lagrangian(env_fn = lambda : gym.make("merge-v0"),ac_kwargs = dict(hidden_sizes=(64,64)))
-ppo(env_fn = lambda : gym.make("merge-v0"),ac_kwargs = dict(hidden_sizes=(64,64)), logger_kwargs = {"output_dir": "./fix_speed_01"}, render=False, max_ep_len=70, epochs=150, steps_per_epoch=5000)
+ppo(env_fn = lambda : gym.make("merge-v0"),ac_kwargs = dict(hidden_sizes=(64,64)), logger_kwargs = {"output_dir": "./speed_0_30_penalty1"}, render=True, max_ep_len=70, epochs=150, steps_per_epoch=5000, penalty_init=1)
