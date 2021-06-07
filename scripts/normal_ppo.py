f42288cb31726179da0181026803cd334f0f4bf7
Danial Kamran <danial.kamran@kit.edu>
2021-06-07 12:26:46 +0200

    refactor normal ppo

--- scripts/normal_ppo.py
+++ scripts/normal_ppo.py
@@ -2,6 +2,14 @@ from safe_rl import ppo_lagrangian, ppo
 import gym
 import highway_env
 
-penalty_init=75
-#ppo_lagrangian(env_fn = lambda : gym.make("merge-v0"),ac_kwargs = dict(hidden_sizes=(64,64)))
-ppo(env_fn = lambda : gym.make("merge-v0"),ac_kwargs = dict(hidden_sizes=(64,64)), logger_kwargs = {"output_dir": "./speed_0_30_penalty_init{}_".format(penalty_init)}, render=False, max_ep_len=70, epochs=150, steps_per_epoch=5000, penalty_init=penalty_init)
+penalty_init = 75
+env_name = "mergefast"
+ppo(env_fn=lambda: gym.make("{}-v0".format(env_name)),
+    ac_kwargs=dict(hidden_sizes=(64, 64)),
+    logger_kwargs={"output_dir": "./normal_ppo_Env{}_P{}_".format(env_name,penalty_init)},
+    render=True,
+    max_ep_len=70,
+    epochs=150,
+    steps_per_epoch=5000,
+    penalty_init=penalty_init
+    )
