9bd9c044f8b89c30862f86f5d6742fc3b2bfcb56
Danial Kamran <danial.kamran@kit.edu>
2021-06-10 16:35:04 +0200

    add new interface for running multiple seeds in normal ppo

--- scripts/normal_ppo.py
+++ scripts/normal_ppo.py
@@ -1,15 +1,31 @@
 from safe_rl import ppo_lagrangian, ppo
 import gym
 import highway_env
+import argparse
 
-penalty_init = 75
-env_name = "mergefast"
-ppo(env_fn=lambda: gym.make("{}-v0".format(env_name)),
-    ac_kwargs=dict(hidden_sizes=(64, 64)),
-    logger_kwargs={"output_dir": "./normal_ppo_Env{}_P{}_".format(env_name,penalty_init)},
-    render=True,
-    max_ep_len=100,
-    epochs=150,
-    steps_per_epoch=5000,
-    penalty_init=penalty_init
-    )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', nargs='*', type=int, default=123)
+    parser.add_argument('--env_name', type=str, default="mergefast-v0")
+    parser.add_argument('--penalty_init', type=float, default=0.01)
+
+    args = parser.parse_args()
+
+    seeds = args.seed if isinstance(args.seed, list) else [args.seed]
+    exps = [
+        dict(
+            env_fn = lambda : gym.make(args.env_name),
+            ac_kwargs = dict(hidden_sizes=(64,64)),
+            logger_kwargs = {"output_dir": f"./results/normal_ppo_Env{args.env_name}_penalty_init_{args.penalty_init}_seed_{seed}"},
+            render=False,
+            max_ep_len=100,
+            epochs=150,
+            steps_per_epoch=5000,
+            penalty_init=args.penalty_init,
+            seed=seed,
+        )
+        for seed in seeds
+    ]
+    for exp_kwargs in exps:
+        ppo(**exp_kwargs)
