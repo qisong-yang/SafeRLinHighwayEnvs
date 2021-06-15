778f2a84832a308f4d7ade37d4de63f2e43619fa
Danial Kamran <danial.kamran@kit.edu>
2021-06-15 14:32:02 +0200

    add other changes for policy evaluation on different environments

--- scripts/test_policy.py
+++ scripts/test_policy.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 from safe_rl.utils.load_utils import load_policy
 from safe_rl.utils.logx import EpochLogger
-
+import gym
 
 
 def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
@@ -12,7 +12,8 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
     assert env is not None, \
         "Environment not found!\n\n It looks like the environment wasn't saved, " + \
         "and we can't run the agent in it. :("
-
+    total_cost = 0.
+    total_length = 0.
     logger = EpochLogger()
     o, r, d, ep_ret, ep_cost, ep_len, n = env.reset(), 0, False, 0, 0, 0, 0
     while n < num_episodes:
@@ -21,15 +22,20 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
             time.sleep(1e-3)
 
         a = get_action(o)
-        a = np.clip(a, env.action_space.low, env.action_space.high)
+        #a = np.clip(a, env.action_space.low, env.action_space.high)
         o, r, d, info = env.step(a)
         ep_ret += r
         ep_cost += info.get('cost', 0)
         ep_len += 1
 
         if d or (ep_len == max_ep_len):
+            if (ep_len==max_ep_len) and d == False:#checking for timeouts
+                ep_cost+=1.
+                print("Timeout!")
             logger.store(EpRet=ep_ret, EpCost=ep_cost, EpLen=ep_len)
-            print('Episode %d \t EpRet %.3f \t EpCost %.3f \t EpLen %d'%(n, ep_ret, ep_cost, ep_len))
+            #print('Episode %d \t EpRet %.3f \t EpCost %.3f \t EpLen %d'%(n, ep_ret, ep_cost, ep_len))
+            total_cost+=ep_cost
+            total_length+= ep_len
             o, r, d, ep_ret, ep_cost, ep_len = env.reset(), 0, False, 0, 0, 0
             n += 1
 
@@ -37,7 +43,7 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
     logger.log_tabular('EpCost', with_min_and_max=True)
     logger.log_tabular('EpLen', average_only=True)
     logger.dump_tabular()
-
+    return total_cost/num_episodes, total_length/num_episodes
 
 if __name__ == '__main__':
     import argparse
@@ -48,8 +54,11 @@ if __name__ == '__main__':
     parser.add_argument('--norender', '-nr', action='store_true')
     parser.add_argument('--itr', '-i', type=int, default=-1)
     parser.add_argument('--deterministic', '-d', action='store_true')
+    parser.add_argument('--env', '-e')
+
     args = parser.parse_args()
     env, get_action, sess = load_policy(args.fpath,
                                         args.itr if args.itr >=0 else 'last',
                                         args.deterministic)
+    env=gym.make(args.env)
     run_policy(env, get_action, args.len, args.episodes, not(args.norender))
