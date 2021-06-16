43a36147062e43691d55b8c660082f7c7b6229f9
Danial Kamran <danial.kamran@kit.edu>
2021-06-16 13:43:43 +0200

    add argument for observation type

--- scripts/normal_ppo.py
+++ scripts/normal_ppo.py
@@ -9,13 +9,14 @@ if __name__ == '__main__':
     parser.add_argument('--seed', nargs='*', type=int, default=123)
     parser.add_argument('--env_name', type=str, default="mergefast-v0")
     parser.add_argument('--penalty_init', type=float, default=0.01)
+    parser.add_argument('--observation', type=str, default="LIST")
 
     args = parser.parse_args()
 
     seeds = args.seed if isinstance(args.seed, list) else [args.seed]
     exps = [
         dict(
-            env_fn = lambda : gym.make(args.env_name),
+            env_fn = lambda : gym.make(args.env_name, observation=args.observation),
             ac_kwargs = dict(hidden_sizes=(64,64)),
             logger_kwargs = {"output_dir": f"./results/normal_ppo_Env{args.env_name}_penalty_init_{args.penalty_init}_seed_{seed}"},
             render=False,
