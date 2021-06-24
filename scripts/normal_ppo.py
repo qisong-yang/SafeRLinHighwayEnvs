cb86b1049ac3624455e067a7a51a7e53ae7ac0de
Danial Kamran <danial.kamran@kit.edu>
2021-06-24 14:20:47 +0200

    fix env names for normal ppo

--- scripts/normal_ppo.py
+++ scripts/normal_ppo.py
@@ -8,7 +8,7 @@ from itertools import product
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', nargs='*', type=int, default=123)
-    parser.add_argument('--env_name', type=str, default="mergefast-v0")
+    parser.add_argument('--env_name',  nargs='*', type=str, default="mergefast-v0")
     parser.add_argument('--penalty_init', type=float, default=0.01)
     parser.add_argument('--observation', type=str, default="LIST")
     parser.add_argument('--coop', nargs='*', type=float, default=0.0)
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     seeds = args.seed if isinstance(args.seed, list) else [args.seed]
-    env_names = args.seed if isinstance(args.env_name, list) else [args.env_name]
+    env_names = args.env_name if isinstance(args.env_name, list) else [args.env_name]
     coops = args.coop if isinstance(args.coop, list) else [args.coop]
 
     def launch_ppo_normal(seed, env_name, coop):
