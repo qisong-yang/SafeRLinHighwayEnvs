e710879ac5e47b2b4ef48b26a46167e9f749bc90
tdsimao <tdsimao@users.noreply.github.com>
2021-06-24 12:03:31 +0200

    run experiments in parallel

---
+++ launcher.sh
@@ -0,0 +1,1 @@
+python scripts/penalized_ppo.py --env_name mergeslow-v0 mergefast-v0 mergemixed-v0  --seed 1 --penalty_lr 0.05 --cost_lim 0.01 --seed 1 2 3 4 5
--- scripts/penalized_ppo.py
+++ scripts/penalized_ppo.py
@@ -2,12 +2,16 @@ from safe_rl import ppo_lagrangian, ppo
 import gym
 import highway_env
 import argparse
+from multiprocessing import Pool
+from itertools import product
+
 
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', nargs='*', type=int, default=123)
-    parser.add_argument('--env_name', type=str, default="mergefast-v0")
+    parser.add_argument('--env_name', nargs='*', type=str, default="mergefast-v0")
     parser.add_argument('--cost_lim', type=float, default=0.01)
     parser.add_argument('--penalty_init', type=float, default=10)
     parser.add_argument('--penalty_lr', type=float, default=0.05)
@@ -17,11 +21,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     seeds = args.seed if isinstance(args.seed, list) else [args.seed]
-    exps = [
-        dict(
-            env_fn = lambda : gym.make(args.env_name, observation=args.observation),
+    env_names = args.env_name if isinstance(args.env_name, list) else [args.env_name]
+
+    def launch_ppo_lagrangian(seed, env_name):
+        ppo_lagrangian(
+            env_fn = lambda : gym.make(env_name, observation=args.observation),
             ac_kwargs = dict(hidden_sizes=(64,64)),
-            logger_kwargs = {"output_dir": f"./results/ppo_lag_{args.env_name}_cost_lim_{args.cost_lim}_penalty_init_{args.penalty_init}_penalty_lr_{args.penalty_lr}_penalty_iters_{args.penalty_iters}_seed_{seed}"},
+            logger_kwargs = {"output_dir": f"./results/ppo_lag_{env_name}_cost_lim_{args.cost_lim}_penalty_init_{args.penalty_init}_penalty_lr_{args.penalty_lr}_penalty_iters_{args.penalty_iters}_seed_{seed}"},
             render=False,
             max_ep_len=100,
             epochs=150,
@@ -32,7 +38,8 @@ if __name__ == '__main__':
             penalty_iters=args.penalty_iters,
             seed=seed,
         )
-        for seed in seeds
-    ]
-    for exp_kwargs in exps:
-        ppo_lagrangian(**exp_kwargs)
+
+    with Pool() as pool:
+        pool.starmap(launch_ppo_lagrangian, product(seeds, env_names))
+
+
