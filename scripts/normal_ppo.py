468e5b5eda4628d53fc1c7df454377029cb0d365
Danial Kamran <danial.kamran@kit.edu>
2021-06-24 13:32:07 +0200

    add arg for cooperativeness of drivers

--- scripts/normal_ppo.py
+++ scripts/normal_ppo.py
@@ -10,16 +10,17 @@ if __name__ == '__main__':
     parser.add_argument('--env_name', type=str, default="mergefast-v0")
     parser.add_argument('--penalty_init', type=float, default=0.01)
     parser.add_argument('--observation', type=str, default="LIST")
+    parser.add_argument('--coop', type=float, default=0.0)
 
     args = parser.parse_args()
 
     seeds = args.seed if isinstance(args.seed, list) else [args.seed]
     exps = [
         dict(
-            env_fn = lambda : gym.make(args.env_name, observation=args.observation),
+            env_fn = lambda : gym.make(args.env_name, observation=args.observation, cooperative_prob=args.coop),
             ac_kwargs = dict(hidden_sizes=(64,64)),
-            logger_kwargs = {"output_dir": f"./results/normal_ppo_Env{args.env_name}_penalty_init_{args.penalty_init}_seed_{seed}"},
-            render=False,
+            logger_kwargs = {"output_dir": f"./results/normal_ppo_Env{args.env_name}_coop{args.coop}_penalty_init_{args.penalty_init}_seed_{seed}"},
+            render=True,
             max_ep_len=100,
             epochs=150,
             steps_per_epoch=5000,
