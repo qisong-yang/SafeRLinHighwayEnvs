58836a812d99900d95631c28afd8a4b0e845c398
Danial Kamran <danial.kamran@kit.edu>
2021-06-24 13:44:49 +0200

    add coop args for parallel penalized ppo

--- scripts/penalized_ppo.py
+++ scripts/penalized_ppo.py
@@ -17,17 +17,19 @@ if __name__ == '__main__':
     parser.add_argument('--penalty_lr', type=float, default=0.05)
     parser.add_argument('--penalty_iters', type=int, default=40)
     parser.add_argument('--observation', type=str, default="LIST")
+    parser.add_argument('--coop', nargs='*', type=float, default=0.0)
 
     args = parser.parse_args()
 
     seeds = args.seed if isinstance(args.seed, list) else [args.seed]
     env_names = args.env_name if isinstance(args.env_name, list) else [args.env_name]
+    coops = args.coop if isinstance(args.coop, list) else [args.coop]
 
-    def launch_ppo_lagrangian(seed, env_name):
+    def launch_ppo_lagrangian(seed, env_name, coop):
         ppo_lagrangian(
-            env_fn = lambda : gym.make(env_name, observation=args.observation),
+            env_fn = lambda : gym.make(env_name, observation=args.observation, cooperative_prob=coop),
             ac_kwargs = dict(hidden_sizes=(64,64)),
-            logger_kwargs = {"output_dir": f"./results/ppo_lag_{env_name}_cost_lim_{args.cost_lim}_penalty_init_{args.penalty_init}_penalty_lr_{args.penalty_lr}_penalty_iters_{args.penalty_iters}_seed_{seed}"},
+            logger_kwargs = {"output_dir": f"./results/ppo_lag_{env_name}_coop{coop}_cost_lim_{args.cost_lim}_penalty_init_{args.penalty_init}_penalty_lr_{args.penalty_lr}_penalty_iters_{args.penalty_iters}_seed_{seed}"},
             render=False,
             max_ep_len=100,
             epochs=150,
@@ -40,6 +42,6 @@ if __name__ == '__main__':
         )
 
     with Pool() as pool:
-        pool.starmap(launch_ppo_lagrangian, product(seeds, env_names))
+        pool.starmap(launch_ppo_lagrangian, product(seeds, env_names, coops))
 
 
