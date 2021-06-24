94b9cb26f0b274ed0ca61a288f6f7d95e443b137
Danial Kamran <danial.kamran@kit.edu>
2021-06-24 14:11:46 +0200

    add parallel execution for normal ppo

--- scripts/normal_ppo.py
+++ scripts/normal_ppo.py
@@ -2,7 +2,8 @@ from safe_rl import ppo_lagrangian, ppo
 import gym
 import highway_env
 import argparse
-
+from multiprocessing import Pool
+from itertools import product
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -10,16 +11,19 @@ if __name__ == '__main__':
     parser.add_argument('--env_name', type=str, default="mergefast-v0")
     parser.add_argument('--penalty_init', type=float, default=0.01)
     parser.add_argument('--observation', type=str, default="LIST")
-    parser.add_argument('--coop', type=float, default=0.0)
+    parser.add_argument('--coop', nargs='*', type=float, default=0.0)
 
     args = parser.parse_args()
 
     seeds = args.seed if isinstance(args.seed, list) else [args.seed]
-    exps = [
-        dict(
-            env_fn = lambda : gym.make(args.env_name, observation=args.observation, cooperative_prob=args.coop),
+    env_names = args.seed if isinstance(args.env_name, list) else [args.env_name]
+    coops = args.coop if isinstance(args.coop, list) else [args.coop]
+
+    def launch_ppo_normal(seed, env_name, coop):
+        ppo(
+            env_fn = lambda : gym.make(env_name, observation=args.observation, cooperative_prob=coop),
             ac_kwargs = dict(hidden_sizes=(64,64)),
-            logger_kwargs = {"output_dir": f"./results/normal_ppo_Env{args.env_name}_coop{args.coop}_penalty_init_{args.penalty_init}_seed_{seed}"},
+            logger_kwargs = {"output_dir": f"./results/normal_ppo/normal_ppo_Env{env_name}_coop{coop}_penalty_init_{args.penalty_init}_seed_{seed}"},
             render=False,
             max_ep_len=100,
             epochs=150,
@@ -27,7 +31,5 @@ if __name__ == '__main__':
             penalty_init=args.penalty_init,
             seed=seed,
         )
-        for seed in seeds
-    ]
-    for exp_kwargs in exps:
-        ppo(**exp_kwargs)
+    with Pool() as pool:
+        pool.starmap(launch_ppo_normal, product(seeds, env_names, coops))
